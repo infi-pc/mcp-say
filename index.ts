f890724df474614bf3dc8a16c5afb85996a09cb0
Michael Musil <infi.pc@gmail.com>
2025-03-07 10:39:27 +0100

    init

---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log("Hello, TypeScript!");
+
+/**
+ * Main function to run the application
+ */
+function main(): void {
+  console.log("Application started");
+  
+  // Your code will go here
+}
+
+// Run the main function
+main();
